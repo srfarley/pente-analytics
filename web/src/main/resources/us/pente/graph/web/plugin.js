c4730d30d7e79d52d41071931c56b19b63ae79f6
Steven Farley <srfarley@gmail.com>
2016-06-04 08:31:18 -0400

    add detail toolbar button to open game page on pente.org

--- web/src/main/resources/us/pente/graph/web/plugin.js
+++ web/src/main/resources/us/pente/graph/web/plugin.js
@@ -18,9 +18,24 @@ require([
         }
     });
 
+    registry.registerExtension('org.visallo.detail.toolbar', {
+        title:  i18n('us.pente.graph.web.detail.toolbar.game'),
+        event: 'openGame',
+        canHandle: function(objects) {
+            return objects.vertices.length === 1 && objects.edges.length === 0
+                && objects.vertices[0].conceptType === 'http://pente.us/pente#game';
+        }
+    });
+
     $(document).on('openPlayerProfile', function(e, data) {
         var playerName = F.vertex.prop(data.vertices[0], 'http://pente.us/pente#playerName');
         var url = 'https://pente.org/gameServer/profile?viewName=' + playerName;
-        window.open(url,'_blank');
+        window.open(url, '_blank');
+    });
+
+    $(document).on('openGame', function(e, data) {
+        var gameId = data.vertices[0].id.substring(5); // id has prefix 'GAME_'
+        var url = 'https://pente.org/gameServer/viewLiveGame?mobile&g=' + gameId;
+        window.open(url, '_blank');
     });
 });
