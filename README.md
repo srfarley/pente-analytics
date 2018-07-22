2452e0be2327898ed5a212efdc4ee7eefea0e031
Steven Farley <srfarley@gmail.com>
2018-07-22 10:09:16 -0400

    Remove Visallo because it's no longer open-source.

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ Visualize and analyze [Pente.org](http://pente.org) games using currently availa
 
 ## Tools
 This repo contains sub-projects for each of the following applications:
-* [Visallo](visallo)
 * [Spark](spark)
--- visallo/auth/src/main/resources/us/pente/graph/auth/plugin.js
+++
@@ -1,9 +0,0 @@
-define(
-    ['configuration/plugins/registry'],
-    function(registry) {
-    'use strict';
-
-    registry.registerExtension('org.visallo.authentication', {
-        componentPath: 'us/pente/graph/auth/authentication'
-    })
-});
